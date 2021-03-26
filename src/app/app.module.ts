33fd4f1969404011fb68241a6c4229025185a93d
George Ramirez <george.ramirez2@gmail.com>
2021-03-26 09:23:01 -0500

    nav bar update without placement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
